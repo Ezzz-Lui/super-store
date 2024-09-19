d1a265c3910a2a7fb44fb27b570f6f26ff5d301a
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-19 06:04:37 -0600

    Refactor product.entity.ts: Update entity properties and add schema

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,7 +1,48 @@
-// Un entity es una tabla (analogia)
-// Declarar explicitamente con su decorador que es una Entity
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-import { Entity } from "typeorm";
+@Entity({schema: 'nestjs_backend'})
+export class Film {
 
-@Entity()
-export class Product {} 
+    @PrimaryGeneratedColumn('uuid')
+    film_id: string;
+
+    @Column('character varying', {
+        unique: true,
+    })
+    title:string;
+
+    @Column('text', {
+        unique: true
+    })
+    description:string;
+
+    @Column('smallint') // Corrección para años
+    release_year:number;
+
+    @Column('smallint')
+    language_id: number;
+
+    @Column('smallint', {
+        default: 3,
+    })
+    rental_duration:number;
+
+    @Column('decimal', { // Corrección para manejar decimales en precios
+        precision: 5,
+        scale: 2,
+        default: 4.99
+    })
+    rental_rate: number;
+
+    @Column('decimal', {
+        precision: 5,
+        scale: 2,
+        default: 19.99
+    })
+    replacement_cost: number;
+
+    @UpdateDateColumn()
+    last_update: Date
+
+
+}
