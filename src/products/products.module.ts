e32822667bd18df06858a63b58298c9469c86f43
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-19 06:04:54 -0600

    Refactor products.module.ts: Add TypeOrmModule and Film entity import

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Film } from './entities/product.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  imports: [TypeOrmModule.forFeature([
+    Film
+  ])]
 })
 export class ProductsModule {}
