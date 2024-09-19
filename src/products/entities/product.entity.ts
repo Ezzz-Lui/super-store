40254218d0d3ace8eb30ad2a48cef174e389022c
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-19 05:01:48 -0600

    Add Entity decorator to Product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,7 @@
-export class Product {}
+// Un entity es una tabla (analogia)
+// Declarar explicitamente con su decorador que es una Entity
+
+import { Entity } from "typeorm";
+
+@Entity()
+export class Product {} 
